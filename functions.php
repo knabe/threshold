fb4209980a8e0849f47e8af02c1a0f3f707fa068
CrystallineDex <alexxr87@gmail.com>
2015-01-22 11:27:14 -0500

    Cleaned up the rest of the customize comments.

--- functions.php
+++ functions.php
@@ -100,21 +100,8 @@ function bones_custom_image_sizes( $sizes ) {
 
 function bones_theme_customizer($wp_customize) {
   // $wp_customize calls go here.
-  //
-  // Uncomment the below lines to remove the default customize sections 
-
-  // $wp_customize->remove_section('title_tagline');
     $wp_customize->remove_section('colors');
     $wp_customize->remove_section('background_image');
-  // $wp_customize->remove_section('static_front_page');
-  // $wp_customize->remove_section('nav');
-
-  // Uncomment the below lines to remove the default controls
-  // $wp_customize->remove_control('blogdescription');
-  
-  // Uncomment the following to change the default section titles
-  // $wp_customize->get_section('colors')->title = __( 'Theme Colors' );
-  // $wp_customize->get_section('background_image')->title = __( 'Images' );
 }
 
 add_action( 'customize_register', 'bones_theme_customizer' );
